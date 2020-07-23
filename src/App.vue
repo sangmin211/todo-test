e70d575180fe06c179f5be39f2abca94adf49b60
sangmin <ppp45081@gmail.com>
2020-07-23 19:42:07 +0900

    todo pulishing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld v-bind:cities="cities" /> -->
+    <Header v-bind:title="pageTitle" />
+    <AddTodo />
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/layouts/Header";
+import Todos from "./components/layouts/Todos";
+import AddTodo from "./components/layouts/AddTodo";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    Header,
+    Todos,
+    AddTodo,
+  },
+  data() {
+    return {
+      // cities: ["서울", "부산", "대구", "대전", "군산", "울산", "광주"],
+      pageTitle: "Todo 앱",
+      todos: [
+        { id: 1, title: "dentist", complete: true },
+        { id: 2, title: "study", complete: false },
+        { id: 3, title: "dinner", complete: true },
+        { id: 4, title: "sleep", complete: true },
+        { id: 5, title: "sleep", complete: false },
+        { id: 6, title: "sleep", complete: false },
+      ],
+    };
+  },
+  methods: {
+    addtodo(): {},
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/layouts/AddTodo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form name="form">
+      <input type="text" placeholder="할 일 입력 ..." name="text" />
+      <input type="submit" value="저장" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
